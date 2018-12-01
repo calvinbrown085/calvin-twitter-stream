fd6248d395ddc6cb4361c4461ff209d5809c10fe
Calvin Brown <calvin.brown@banno.com>
2018-12-01 12:33:22 -0600

    changing var names around

--- src/main/scala/com/calvin/twitter/streamer/Config.scala
+++ src/main/scala/com/calvin/twitter/streamer/Config.scala
@@ -9,10 +9,10 @@ object Config {
   final case class Auth(accessSecret: String, accessToken: String, consumerKey: String, consumerSecret: String, port: Int)
 
   def loadConfig[F[_]](implicit F: Effect[F]): F[Auth] = F.delay{
-    val accessSecret = scala.util.Properties.envOrElse("accessSecret", "bad")
-    val accessToken = scala.util.Properties.envOrElse("accessToken", "bad")
-    val consumerKey = scala.util.Properties.envOrElse("consumerKey", "bad")
-    val consumerSecret = scala.util.Properties.envOrElse("consumerSecret", "bad")
+    val accessSecret = scala.util.Properties.envOrElse("ACCESS_SECRET", "fail if we don't have this value")
+    val accessToken = scala.util.Properties.envOrElse("ACCESS_TOKEN", "fail if we don't have this value")
+    val consumerKey = scala.util.Properties.envOrElse("CONSUMER_KEY", "fail if we don't have this value")
+    val consumerSecret = scala.util.Properties.envOrElse("CONSUMER_SECRET", "fail if we don't have this value")
     val port = scala.util.Properties.envOrElse("PORT", "8080")
     Auth(accessSecret = accessSecret, accessToken = accessToken, consumerKey = consumerKey, consumerSecret = consumerSecret, port = port.toInt)
   }
